28c6c431eadb2abf7b26dbcbae44d3ba4642e17e
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 12:36:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,5 @@
 def roll_call_dwarves(dwarves)
-  dwarves.each_with_index { |dwarf, idx| }
-  puts "#{index +1} {dwarf}"
+  dwarves.each_with_index { |dwarf, idx| puts "#{index +1} {dwarf}" }
 end
 
 
