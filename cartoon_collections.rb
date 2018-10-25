4a0d08d2b31aa3d063d9bb9c74c773c865f9c267
Susanne Lundkvist <susanne.r.lundkvist@gmail.com>
2018-10-25 13:08:10 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,7 +19,17 @@ return false
 end
    
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(food)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  food.find do |food|
+    if 
 end
+
+
+cheese = ["gouda", "cheddar", "camembert"]
+  foods.find do |food| 
+    if cheese.include? food 
+      return food
+    end
+  end
+  return nil 
